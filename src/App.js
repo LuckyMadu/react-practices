d4746b0399241f50b7b32d75baca0a33c7aec303
unknown <you@example.com>
2019-05-24 22:09:20 +0530

    Applying styles

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from "react";
 
 function App() {
   const state = {
-    count: 0
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
 
   function formatCount() {
     const { count } = state;
-    return count === 0 ? <h3>zero</h3> : count;
+    return count === 0 ? 'zero': count;
   }
+
+  const styles = {
+    fontSize: 14,
+    fontWeight: "bold",
+    paddingLeft:5,
+    paddingRight:5
+  };
+
   return (
     <div className="App">
-      <span>{formatCount()}</span>
-      <button>Increment</button>
+      <img src={state.imageUrl} alt="" />
+      <span style={styles} className="badge badge-primary m-2">
+        {formatCount()}
+      </span>
+      <button style={{fontSize:20}} className="btn btn-secondary btn-md">Increment</button>
     </div>
   );
 }
