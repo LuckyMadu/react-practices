48aefd34172e1a556ed22e1679ab858a20320b78
unknown <you@example.com>
2019-05-25 18:47:34 +0530

    colored badges with different values

--- src/App.js
+++ src/App.js
@@ -2,31 +2,28 @@ import React from "react";
 
 function App() {
   const state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200"
+    count: 1
   };
 
   function formatCount() {
     const { count } = state;
-    return count === 0 ? 'zero': count;
+    return count === 0 ? "zero" : count;
   }
 
-  const styles = {
-    fontSize: 14,
-    fontWeight: "bold",
-    paddingLeft:5,
-    paddingRight:5
-  };
-
   return (
-    <div className="App">
-      <img src={state.imageUrl} alt="" />
-      <span style={styles} className="badge badge-primary m-2">
-        {formatCount()}
-      </span>
-      <button style={{fontSize:20}} className="btn btn-secondary btn-md">Increment</button>
+    <div>
+      <span className={getBadgeClasses()}>{formatCount()}</span>
+      <button style={{ fontSize: 20 }} className="btn btn-secondary btn-md">
+        Increment
+      </button>
     </div>
   );
+
+  function getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
 
 export default App;
