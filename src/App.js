2d7af6ae88e2967e59ec4459bd86b2489e206bc8
unknown <you@example.com>
2019-05-26 10:26:22 +0530

    if else in jsx

--- src/App.js
+++ src/App.js
@@ -3,33 +3,26 @@ import React from "react";
 function App() {
   const state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
-  function formatCount() {
-    const { count } = state;
-    return count === 0 ? "zero" : count;
-  }
+  function renderTags() {
+    if (state.tags.length === 0) return <p>There are no tags!</p>;
 
-  return (
-    <div>
-      <span className={getBadgeClasses()}>{formatCount()}</span>
-      <button style={{ fontSize: 20 }} className="btn btn-secondary btn-md">
-        Increment
-      </button>
+    return (
       <ul>
         {state.tags.map(tag => (
           <li key={tag}>{tag}</li>
         ))}
       </ul>
+    );
+  }
+  return (
+    <div>
+      {state.tags.length === 0 && "Please create a new tag!"}
+      {renderTags()}
     </div>
   );
-
-  function getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
 }
 
 export default App;
