cd54dac4258792a9f0339751823da828536363db
unknown <you@example.com>
2019-05-27 17:59:40 +0530

    function instead of class

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </nav>
-      </div>
-    );
-  }
-}
+//stateless functional components
+const Navbar = props => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
